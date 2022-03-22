64818fb2ce0a67ed6150f4b209ad3ea1d01e5620
danielcope <daniel.ash.cope@gmail.com>
2022-03-22 10:39:56 -0600

    committing small changes

---
+++ README.md
@@ -0,0 +1,14 @@
+##Hi!
+
+This is just for fun. This is where I can testing things out in development.
+
+###Tech
+> - react (17.0.2)
+> - react-scripts (5.0.0)
+> - react-redux (7.2.6)
+> - react-router (5.2.1)
+> - aos (2.3.4)
+> - axios (0.21.4)
+> - dotenv (10.0.0)
+> - express (4.17.3)
+> - express-session (1.17.2)
--- src/components/Elephant.js
+++ src/components/Elephant.js
@@ -4,7 +4,6 @@ import blahface from "../assets/img/face-removebg-preview.png";
 function Face() {
   return (
     <section className="h-full flex justify-center items-center">
-      {/* <FontAwesomeIcon icon={faFaceGrin} className="elephant" /> */}
       <img src={blahface} alt="face" className="elephant" />
     </section>
   );
